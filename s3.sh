bf1b6709292c5b80effcc677df58e46e5b6b487f
Martinx <martincanencia@gmail.com>
2026-01-22 00:58:23 +0000

    8

--- s3.sh
+++ s3.sh
@@ -1,4 +1,10 @@
 # 1. Crear un archivo simple con la letra
-echo -n "a" > letra.txt
+echo -n "Mensaje secreto" > letra.txt
 # 2. Ver su representación en bits (usando xxd para volcado binario)
 xxd -b letra.txt
+# Ver permisos actuales (mira la primera columna, ej: -rw-r--r--)
+ls -l letra.txt
+# Cambiar a permisos 755 (rwx para dueño, r-x para grupo, r-x para otros)
+chmod 755 letra.txt
+# Verificar cambiols 
+-l letra.txt
