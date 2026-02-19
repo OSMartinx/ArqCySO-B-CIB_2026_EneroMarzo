c155619e09c5f575e9fb448b38a4f4bddca3033d
Martinx <martincanencia@gmail.com>
2026-02-19 01:51:14 +0000

    f

--- script.ps1
+++ script.ps1
@@ -2,3 +2,28 @@ $c='chao'
 Write-Host $c
 Write-Output $c
 Write-Host "Hola" -ForegroundColor Green -BackgroundColor Black
+[int] $x = 10
+if ($x -gt 5) {
+Write-Host "Mayor que 5"
+} else {
+Write-Host "5 o menos"
+}
+[int]$x = 10
+
+if ($x -gt 5) {
+Write-Host "Mayor que 5"
+} else {
+
+Write-Host "5 o menos"
+
+}
+ 
+$lista = @(1, 2, 3)
+foreach ($item in $lista) {
+    Write-Host $item
+}
+$x = 0
+while ($x -lt 3) {
+Write-Host $x
+$x++
+}
